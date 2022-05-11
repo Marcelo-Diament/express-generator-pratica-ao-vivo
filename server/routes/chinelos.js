b6d562fe313f300051130bfd9ae1ce5b67d617c8
Marcelo-Diament <marcelodiament@gmail.com>
2022-05-11 20:32:10 -0300

    feat: adiciona ChinelosController

--- server/routes/chinelos.js
+++ server/routes/chinelos.js
@@ -1,8 +1,7 @@
 const express = require('express')
+const controller = require('../controllers/ChinelosController')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.send("Só as chinela")
-})
+router.get('/', controller.index)
 
 module.exports = router
